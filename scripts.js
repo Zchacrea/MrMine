a0bcf6c7ca50aad769f2ec077197e9777949b6e0
Zchacrea <adam@blueverie.com>
2014-03-20 10:15:59 -0400

    scripts.js

---
+++ scripts.js
@@ -0,0 +1,30 @@
+//To be used with the MrMine Webgame under the console.  Copy source in full then check readme for how to use.
+
+var sellall = function()
+{
+  document.getElementById('UPALLB').onclick() //brings us to top;
+  document.getElementById('PLACE1').onclick() //opens sell center;
+  document.getElementById('SB18').onclick()   //sells all minerals;
+  document.getElementById('CLOSEs').onclick() //closes sell center;
+}
+
+var findchests = function()
+{
+  document.getElementById('UPALLB').onclick()         //brings us to top;
+  letters = ['a','b','c','d','e','f','g','h','i','j'] //array to cycle through miners
+  i=0;
+  while (i<505)                                       //current Maximum Depth
+  {
+    docuemnt.getElementById('DOWNB').onclick()        //takes us down one level
+    letters.forEach(
+      function(e)
+        {
+          document.getElementById('L5'+e).onclick()   //clicks chest if over a miner
+          document.getElementById('OPENTIT').onclick()//opens chest
+          document.getElementById('OPENTIT').onclick()//closes chest
+        }
+      )
+    i++;
+  }
+}
+
